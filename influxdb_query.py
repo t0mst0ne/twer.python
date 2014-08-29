76dc4312621dd85c843b664b36812d8cc3166c31
tomstone <mac@Mini.local>
2014-08-29 23:57:08 +0800

    Put hospital_sn to columns and plot time-pending_ward grouped by hospital_sn

--- influxdb_query.py
+++ influxdb_query.py
@@ -15,15 +15,13 @@ result = db.query('select last(pending_ward) as ward, DIFFERENCE(pending_ward) a
 # result[0]['columns'] provides column name
 # result[0]['points'] provides the list data
 #data = pd.DataFrame(A, columns=col_name)
-print result[0]['columns'][0]
-data = pd.DataFrame(result[0]['points'], columns=result[0]['columns'])
-#print data
+#print result[0]['columns'][0]
+
+data = pd.DataFrame(result[0]['points'], columns=result[0]['columns'])  # 把 sql query 回來的讀進 dataframe
+table = pd.tools.pivot.pivot_table(data, index=['time'], columns=['hospital_sn'])  # 把 hospital_sn 拉到 columns
+print table.plot()   
+
 
 A = data.ix[data['hospital_sn'] == '0401180014']   # 指定某SN
-#print A
-
 A['time'] = [datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S') for x in A['time']]  # 把 timestamp 轉成 datatime
-
-A.plot(x='time', y='ward')   # plot 指定一個column 當X , 另一個當Y
-
-
+print A.plot(x='time', y='ward')   # plot 指定一個column 當X , 另一個當Y`
