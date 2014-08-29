384ef511e2107de517c10ccb9fc61db21d8ed9be
tomstone <mac@Mini.local>
2014-08-29 23:19:55 +0800

    change timestamp to datatime plot with X = datatime , Y = pending_ward

--- influxdb_query.py
+++ influxdb_query.py
@@ -1,5 +1,6 @@
 from influxdb import client as influxdb
 import pandas as pd
+import datetime
 
 host = 'api-beta.ly.g0v.tw'
 port = '8086'
@@ -14,10 +15,15 @@ result = db.query('select last(pending_ward) as ward, DIFFERENCE(pending_ward) a
 # result[0]['columns'] provides column name
 # result[0]['points'] provides the list data
 #data = pd.DataFrame(A, columns=col_name)
-
+print result[0]['columns'][0]
 data = pd.DataFrame(result[0]['points'], columns=result[0]['columns'])
+#print data
 
-A = data.ix[data['hospital_sn'] == '0401180014']
-print A
-A[['ward']].plot()
+A = data.ix[data['hospital_sn'] == '0401180014']   # 指定某SN
+#print A
+
+A['time'] = [datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S') for x in A['time']]  # 把 timestamp 轉成 datatime
+
+A.plot(x='time', y='ward')   # plot 指定一個column 當X , 另一個當Y
+
 
